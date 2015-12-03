c73c14b569b6f5609a4817e766a0b68e642fb167
Per Thomas Jahr <perja@extern.uio.no>
2015-12-03 21:58:49 +0100

    First pages commit.

---
+++ bt.js
@@ -0,0 +1,30 @@
+document.querySelector('button').addEventListener('click', function() {
+    if (!navigator.bluetooth) {
+	console.log('Web Bluetooth API is not available.\n' +
+		    'Please make sure the Web Bluetooth flag is enabled.');
+    } else {
+	onButtonClick();
+    }
+});
+
+function onButtonClick() {
+    'use strict';
+    console.log('Requesting Bluetooth Device...');
+    
+    navigator.bluetooth.requestDevice({
+	filters: [{
+	    services: [0xa495ff20c5b14b44b5121370f02d74de]
+	}]
+    })
+	.then(device => device.connectGATT())
+	.then(server => {
+	    return server.getPrimaryService(0xa495ff20c5b14b44b5121370f02d74de);
+	})
+	.then(service => {
+	    var lock = new Uint8Array([1]);
+	    return characteristic.writeValue(lock);
+	})
+	.then(device => device.disconnect())
+	.catch(error => { console.log(error); });
+}
+   
