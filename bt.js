0b1e5e8494ad16fb038aecf5cded4774dcb169e2
Per Thomas Jahr <perja@extern.uio.no>
2015-12-05 22:14:34 +0100

    Try to choose device only once.

--- bt.js
+++ bt.js
@@ -1,3 +1,5 @@
+var devicePromise;
+
 document.querySelector('.lock').addEventListener('click', function() {
     onButtonClick(1);
 });
@@ -5,6 +7,10 @@ document.querySelector('.lock').addEventListener('click', function() {
 document.querySelector('.unlock').addEventListener('click', function() {
     onButtonClick(0);
 });
+
+function requestDevice() {
+    return navigator.bluetooth.requestDevice({filters: [{name: "mystery box"}]});
+}
 
 function onButtonClick(lock) {
     'use strict';
@@ -17,13 +23,15 @@ function onButtonClick(lock) {
 
     console.log('Requesting Bluetooth Device...');
 
-    navigator.bluetooth.requestDevice({filters: [{name: "mystery box"}]})
-	.then(device => device.connectGATT())
+    devicePromise = devicePromise || requestDevice();
+    
+    devicePromise.then(device => device.connectGATT())
 	.then(server => server.getPrimaryService('a495ff20-c5b1-4b44-b512-1370f02d74de'))
 	.then(service => service.getCharacteristic('a495ff21-c5b1-4b44-b512-1370f02d74de'))
 	.then(characteristic => {
 	    return characteristic.writeValue(new Uint8Array([lock]));
 	})
+	.then(server => disconnect())
 	.then(() => {
 	    console.log('Success!');
 	})
