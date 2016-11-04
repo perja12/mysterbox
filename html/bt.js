e00341edc7968e8178a5293c2e124b657121f107
Per Thomas Jahr <perja@opera.com>
2016-11-04 14:17:03 +0100

    WIP: Attempt to handle brute forcing the pin. Not working as intended.

--- html/bt.js
+++ html/bt.js
@@ -1,30 +1,94 @@
-document.querySelector('button').addEventListener('click', function() {
-    if (!navigator.bluetooth) {
-	console.log('Web Bluetooth API is not available.\n' +
-		    'Please make sure the Web Bluetooth flag is enabled.');
-    } else {
-	onButtonClick();
-    }
-});
-
-function onButtonClick() {
+(function() {
     'use strict';
-    console.log('Requesting Bluetooth Device...');
-    
-    navigator.bluetooth.requestDevice({
-	filters: [{
-	    services: [0xa495ff20c5b14b44b5121370f02d74de]
-	}]
-    })
-	.then(device => device.connectGATT())
-	.then(server => {
-	    return server.getPrimaryService(0xa495ff20c5b14b44b5121370f02d74de);
-	})
-	.then(service => {
-	    var lock = new Uint8Array([1]);
-	    return characteristic.writeValue(lock);
-	})
-	.then(device => device.disconnect())
-	.catch(error => { console.log(error); });
-}
-   
+    var devicePromise;
+
+    const SERVICE = 'a495ff20-c5b1-4b44-b512-1370f02d74de';
+    const ADVERTISED_SERVICE = 'a495ff10-c5b1-4b44-b512-1370f02d74de';
+    const PIN_CHARACTERISTIC = 'a495ff21-c5b1-4b44-b512-1370f02d74de';
+    const RESULT_CHARACTERISTIC = 'a495ff22-c5b1-4b44-b512-1370f02d74de';
+
+    var pinElement = document.querySelector('#pin');
+    var formElement = document.querySelector('#form');
+    var resultElement = document.querySelector('#result');
+
+    document.querySelector('#lock').addEventListener('click', function() {
+        resultElement.innerHTML = '&nbsp;';
+        lockBox(true);
+    });
+
+    document.querySelector('#unlock').addEventListener('click', function() {
+        resultElement.innerHTML = '&nbsp;';
+        if (pinElement.checkValidity()) {
+            lockBox(false);
+        }
+    });
+
+    function requestDevice() {
+        console.log('Requesting Bluetooth Device...');
+        return navigator.bluetooth.requestDevice(
+            {filters: [{services: [ADVERTISED_SERVICE]}],
+             optionalServices: [SERVICE]
+            });
+    }
+
+    function lockBox(lock) {
+        'use strict';
+        let gattServer;
+        let primaryService;
+
+        if (!navigator.bluetooth) {
+            console.log('Web Bluetooth API is not available.\n' +
+                        'Please use a compatible browser for Android (Opera Beta or Chrome) and make sure the Web Bluetooth flag is enabled (opera:flags or chrome:flags).');
+            return;
+        }
+
+        devicePromise = devicePromise || requestDevice();
+        devicePromise
+            .then(device => {
+                return device.gatt.connect();
+            }).
+            then(server => {
+                gattServer = server;
+                return server.getPrimaryService(SERVICE);
+            })
+            .then(service => {
+                primaryService = service;
+                return service.getCharacteristic(PIN_CHARACTERISTIC);
+            })
+            .then(characteristic => {
+                var value = (lock ? 0 : parseInt(pinElement.value, 10));
+
+                // Arduino based boards uses little endian while js
+                // uses the endianness of the cpu.
+                var buffer = new ArrayBuffer(4);
+                new DataView(buffer).setUint32(0 , value, /*littleEndian=*/ true);
+                return characteristic.writeValue(new Uint32Array(buffer));
+            })
+            .then(function() {
+                return new Promise(function(resolve, reject) {
+                    setTimeout(function() {
+                        resolve(0);
+                    }, 1000);
+                })
+            })
+            .then(() => {
+                return primaryService.getCharacteristic(RESULT_CHARACTERISTIC);
+            })
+            .then(characteristic => {
+                return characteristic.readValue();
+            })
+            .then(value => {
+                let result = value.getUint32(0, true);
+                console.log('Result: ' + result);
+                resultElement.innerHTML = result ? 'Locked' : 'Unlocked!';
+            })
+            .then(() => {
+                formElement.reset();
+                // Disconnect to save power.
+                return gattServer.disconnect();
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+} ());
--- mysterybox/mysterybox.ino
+++ mysterybox/mysterybox.ino
@@ -3,63 +3,127 @@
 Servo servo;
 
 // The top secret pin code
-const uint32_t pinCode = 43215; 
+const uint32_t pin_code = 43215;
 
 // Pins
-const int servoPin = 1;
-const int ledPin = 5;
+const int servo_pin = 1;
+const int led_pin = 5;
 
 // Values for the servo
-const int valLocked = 72;
-const int valUnlocked = 106;
+const int val_locked = 72;
+const int val_unlocked = 106;
 
-// The scratch bank to use
-const int bank = 1;
+// The scratch banks to use
+const int pin_bank = 1;
+const int result_bank = 2;
 
 static bool connected = false;
 static bool locked = false;
 
+const int lock_error = 0;
+const int lock_success = 1;
+
+int attempts = 0;
+unsigned long stop_ignore = 0;
+long old_value = 0;
+
 void setup() {
-  Bean.enableConfigSave(false);
-  Bean.enableWakeOnConnect(true);
-  Bean.setAdvertisingInterval(500);
-  pinMode(ledPin, OUTPUT);
-  
-  // Lock the box when powering on
-  lock(true);
+    Bean.enableConfigSave(false);
+    Bean.enableWakeOnConnect(true);
+    Bean.setAdvertisingInterval(250);
+    pinMode(led_pin, OUTPUT);
+
+    // Lock the box when powering on
+    lock(true);
+}
+
+bool isBruteForceTimeoutActive() {
+    unsigned long now = millis();
+    Bean.setScratchNumber(3, now);
+    Bean.setScratchNumber(4, stop_ignore);
+
+    if (millis() < stop_ignore) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void checkBruteForce() {
+    if (attempts > 0 && (attempts % 10 == 0)) {
+        stop_ignore = (unsigned long)(millis() + 5000);
+    } else {
+        stop_ignore = 0;
+    }
 }
 
 void loop() {
-  Bean.setLed(0, 0, 255);
-  connected = Bean.getConnectionState();
+    Bean.setLed(0, 0, 255);
+    connected = getConnectionState();
 
-  if (!connected) {
-    // Turn off led and go to sleep if there is no active connection.
-    digitalWrite(ledPin, LOW);
-    Bean.sleep(0xffffffff);
-  } else {
-    // There is an active connection: 
-    // 1) Read from scratch 1 (0 = lock, pin=unlock)
-    // 2) Sleep 100 and then goto 1 until a disconnect.
-    digitalWrite(ledPin, HIGH);
-    long state = Bean.readScratchNumber(bank);
-    if (state == 0 && !locked) {
-      lock(true);
-    } else if (state == pinCode && locked) {
-      lock(false);
-      // Hide the pin code so that the next client can't read it
-      Bean.setScratchNumber(bank, 1);
+    if (connected) {
+        // Turn the led around the power button on
+        digitalWrite(led_pin, HIGH);
+
+        // Check if the scratch value has changed
+        long value = Bean.readScratchNumber(pin_bank);
+        if (value != old_value) {
+            Bean.setLed(0, 255, 0);
+            delay(200);
+            attempts++;
+
+            old_value = value;
+            // There is a new value - first check if the brute force timeout is active
+            if (isBruteForceTimeoutActive()) {
+                Bean.setLed(255, 0, 0);
+                Bean.sleep(100);
+                return;
+            }
+
+            checkBruteForce();
+
+            // If not we check if this is an attempt to unlock or lock
+            if (locked && value != 0) { // UNLOCK
+                if (value == pin_code) {
+                    lock(false);
+                    // Hide the pin code so that the next client can't read it
+                    Bean.setScratchNumber(pin_bank, -1);
+                    attempts = 0;
+                }
+            } else if (!locked && value == 0) { // LOCK
+                lock(true);
+            }
+        }
+    } else {
+        // We are not connected - turn off led and go to sleep if there is no active connection.
+        digitalWrite(led_pin, LOW);
+        Bean.sleep(0xffffffff);
     }
-  }
-  Bean.sleep(100);
+    Bean.sleep(100);
 }
 
 void lock(bool lock) {
-  servo.attach(servoPin);
-  servo.write(lock ? valLocked : valUnlocked);
-  Bean.setLed(lock ? 255 : 0, lock ? 0 : 255, 0);
-  delay(500);
-  locked = lock;
-  servo.detach();
+    servo.attach(servo_pin);
+    servo.write(lock ? val_locked : val_unlocked);
+    for (int i = 0; i < 5; i++) {
+        Bean.setLed(lock ? 255 : 0, lock ? 0 : 255, 0);
+        delay(100);
+        Bean.setLed(0, 0, 0);
+        delay(100);
+    }
+    locked = lock;
+    servo.detach();
+    Bean.setScratchNumber(result_bank, (locked ? 1 : 0));
 }
 
+// Workaround for bug in firmware.
+// See: http://beantalk.punchthrough.com/t/wrong-result-from-bean-getconnectionstate/1089/7
+bool getConnectionState() {
+    int count = 0;
+    bool connected = Bean.getConnectionState();
+    while (!connected && (count++ < 2)) {
+        delay(5);
+        connected = Bean.getConnectionState();
+    }
+    return connected;
+}
